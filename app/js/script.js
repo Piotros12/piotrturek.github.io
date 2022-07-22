43b5e2ad5f3ea13a175b5bfe07ec6509ce24f672
Piotr Turek <piotrekturek286@gmail.com>
2022-07-22 16:43:15 +0200

    starting creating new hamburger menu using JS

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,18 @@
-console.log('hello world!')
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
+
+const handleClick = () => {
+  hamburger.classList.toggle('hamburger--active');
+  nav.classList.toggle('navigation--active');
+}
+
+hamburger.addEventListener('click', handleClick);
+
+
+
+function myFunction() {
+    var element = document.getElementById("hamburger__click");
+    var element2 = document.getElementById("navigation__click");
+    element.classList.toggle("hamburger--active");
+    element2.classList.toggle("navigation--active");
+  }
