bd3b59789d0cea78a0f7ac81bc2ed01a892c4fb7
Piotr Turek <piotrekturek286@gmail.com>
2022-07-26 12:37:48 +0200

    Improved hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -6,3 +6,15 @@ function myFunction() {
     element2.classList.toggle("navigation--active");
     element3.classList.toggle("header__logo--active");
   }
+
+  function logoClosingMenu() {
+    var element = document.getElementById("hamburger__click");
+    var element2 = document.getElementById("navigation__click");
+    var element3 = document.getElementById("header__logo__click");
+    if (element3.classList.contains('header__logo--active'))
+      {
+        element.classList.toggle("hamburger--active");
+        element2.classList.toggle("navigation--active");
+        element3.classList.toggle("header__logo--active");
+      }
+  }
