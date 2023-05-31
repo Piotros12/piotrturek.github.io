545f721e905dee3323954d6f85f0feb0679966ae
Piotrek PC <piotrekturek286@gmail.com>
2023-05-31 19:19:59 +0200

    corrected some things and added switching plans prices

--- multi-form/multi-form.js
+++ multi-form/multi-form.js
@@ -3,7 +3,10 @@ let tabs = document.getElementsByClassName("step0");
 let number = document.getElementsByClassName("number");
 const previousButton = document.getElementById("button-previous");
 const nextButton = document.getElementById("button-next");
+const form = document.querySelector("form");
+const switchButton = document.getElementById("switch__button");
 
+// FORM TABS HANDLING
 window.onload = showTab(currentTab);
 
 function showTab(currentTab) {
@@ -34,3 +37,43 @@ function previousStep(event) {
   currentTab = currentTab - 1;
   showTab(currentTab);
 }
+
+// SWITCHING PRICES BETWEEN MO?YR PLANS HANDLING
+let isYearly = false;
+let monthlyPrices = document.querySelectorAll("[data-plan='monthly'");
+let yearlyPrices = document.querySelectorAll("[data-plan='yearly'");
+
+switchButton.addEventListener("change", switchHandle);
+
+window.onload = switchHandle();
+
+function switchHandle() {
+  if (switchButton.checked) {
+    isYearly = true;
+    console.log("isYearly? " + isYearly);
+  } else if (!switchButton.checked) {
+    isYearly = false;
+    console.log("isYearly? " + isYearly);
+  }
+  changePrices();
+}
+
+function changePrices() {
+  if (isYearly) {
+    monthlyPrices.forEach((i) => {
+      i.style.display = "none";
+    });
+    yearlyPrices.forEach((i) => {
+      i.style.display = "block";
+    });
+  } else if (!isYearly) {
+    monthlyPrices.forEach((i) => {
+      i.style.display = "block";
+    });
+    yearlyPrices.forEach((i) => {
+      i.style.display = "none";
+    });
+  }
+}
+
+// SUMMARY GENERATING HANDLING
