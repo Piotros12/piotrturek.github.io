08da195427be898479d68cdb20e8877a6b1245a9
Piotrek PC <piotrekturek286@gmail.com>
2023-07-14 10:11:52 +0200

    new portfolio started

--- app/js/script.js
+++ app/js/script.js
@@ -1,51 +1,56 @@
-function myFunction() {
-  var element = document.getElementById("hamburger__click");
-  var element2 = document.getElementById("navigation__click");
-  var element3 = document.getElementById("header__logo__click");
-  element.classList.toggle("hamburger--active");
-  element2.classList.toggle("navigation--active");
-  element3.classList.toggle("header__logo--active");
-}
-
-function logoClosingMenu() {
-  var element = document.getElementById("hamburger__click");
-  var element2 = document.getElementById("navigation__click");
-  var element3 = document.getElementById("header__logo__click");
-  if (element3.classList.contains("header__logo--active")) {
-    element.classList.toggle("hamburger--active");
-    element2.classList.toggle("navigation--active");
-    element3.classList.toggle("header__logo--active");
-  }
-}
-
-let i = 0;
-let isDeleting = false;
-let text = "freelancerem";
-const delay = 3000;
-
-const typing = setInterval(() => {
-  const animatedText = document.querySelector(".animated-text");
-
-  if (isDeleting) {
-    animatedText.textContent = text.substring(0, i);
-    i--;
-    if (i === -1) {
-      isDeleting = false;
-      text = text === "freelancerem" ? "designerem" : "freelancerem";
-      i = 0;
-    }
+let hamburgerIsOpened = false;
+let hamburgerButton = document.getElementById("hamburger__click");
+let navigationList = document.getElementById("navigation__click");
+let logo = document.getElementById("header__logo__click");
+function handleHamburgerButton() {
+  if (hamburgerIsOpened) {
+    hamburgerButton.classList.remove("hamburger--active");
+    navigationList.classList.remove("navigation--active");
+    logo.classList.remove("header__logo--active");
+    hamburgerIsOpened = false;
   } else {
-    if (i < text.length) {
-      animatedText.textContent += text[i];
-      i++;
-    }
-    if (i === text.length) {
-      setTimeout(() => {
-        isDeleting = true;
-      }, delay);
-    }
+    hamburgerButton.classList.add("hamburger--active");
+    navigationList.classList.add("navigation--active");
+    logo.classList.add("header__logo--active");
+    hamburgerIsOpened = true;
   }
-}, 50);
+}
+
+function closeHamburger() {
+  hamburgerButton.classList.remove("hamburger--active");
+  navigationList.classList.remove("navigation--active");
+  logo.classList.remove("header__logo--active");
+  hamburgerIsOpened = false;
+}
+
+// let i = 0;
+// let isDeleting = false;
+// let text = "freelancerem";
+// const delay = 3000;
+
+// const typing = setInterval(() => {
+//   const animatedText = document.querySelector(".animated-text");
+
+//   if (isDeleting) {
+//     animatedText.textContent = text.substring(0, i);
+//     i--;
+//     if (i === -1) {
+//       isDeleting = false;
+//       text = text === "freelancerem" ? "designerem" : "freelancerem";
+//       i = 0;
+//     }
+//   } else {
+//     if (i < text.length) {
+//       animatedText.textContent += text[i];
+//       i++;
+//     }
+//     if (i === text.length) {
+//       setTimeout(() => {
+//         isDeleting = true;
+//       }, delay);
+//     }
+//   }
+// }, 50);
 
 // PROJECTS CARRUSEL
 const projectsList = document.querySelector("#projects-carusell ul");
